9159986d54fd68b049602af904987ca4486b8396
Prakhar Kumar Kashyap <prakharkumar506978@gmail.com>
2021-04-22 21:30:11 +0530

    22-04-2021

--- js/app.js
+++ js/app.js
@@ -1,3 +1,97 @@
+// tsParticles
+//   .load("tsparticles", {
+//     background: {
+//       color: {
+//         value: "#000"
+//       }
+//     },
+//     fpsLimit: 120,
+//     particles: {
+//       number: {
+//         value: 100,
+//         density: {
+//           enable: false,
+//           value_area: 800
+//         }
+//       },
+//       color: {
+//         value: ["#5bc0eb", "#fde74c", "#9bc53d", "#e55934", "#fa7921"]
+//       },
+//       shape: {
+//         type: "circle"
+//       },
+//       opacity: {
+//         value: 1
+//       },
+//       size: {
+//         value: 32,
+//         random: {
+//           enable: true,
+//           minimumValue: 16
+//         }
+//       },
+//       line_linked: {
+//         enable: false,
+//         distance: 150,
+//         color: "#ffffff",
+//         opacity: 0.4,
+//         width: 1
+//       },
+//       move: {
+//         enable: true,
+//         speed: 1,
+//         direction: "none",
+//         random: false,
+//         straight: false,
+//         outMode: "out",
+//         bounce: false,
+//         warp: false,
+//         noise: {
+//           enable: true,
+//           delay: {
+//             value: 0.1
+//           }
+//         }
+//       }
+//     },
+//     interactivity: {
+//       detect_on: "canvas",
+//       events: {
+//         onHover: {
+//           enable: true,
+//           mode: "push"
+//         },
+//         resize: true
+//       }
+//     },
+//     detectRetina: true,
+//     pauseOnBlur: true
+//   })
+//   .then((container) => {
+//     container.setNoise({
+//       init: function () {},
+//       update: function () {},
+//       generate: function (p) {
+//         if (!p.noiseAngle) {
+//           p.noiseAngle = 0;
+//         } else if (p.noiseAngle >= Math.PI * 2) {
+//           p.noiseAngle -= Math.PI * 2;
+//         }
+
+//         const pos = p.getPosition();
+
+//         p.noiseAngle += Math.PI * 2 * Math.random();
+
+//         return {
+//           angle: p.noiseAngle,
+//           length: Math.random() * 2 - 1
+//         };
+//       }
+//     });
+
+//     container.refresh();
+//   });
+
 tsParticles.load("tsparticles", {
   detectRetina: true,
   fpsLimit: 60,
